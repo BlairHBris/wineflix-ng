5e1c07a1b1321859689793b77a960afc6f602bd8
Blair Bristow <blairhbristow@gmail.com>
2022-05-16 16:00:17 -0400

    Create nav link component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { NavigationLinksComponent } from './components/navigation-links/navigation-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationLinksComponent
   ],
   imports: [
     BrowserModule,
